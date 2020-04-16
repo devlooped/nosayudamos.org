4306bbf5c45f4d0602d419be7435d3f65c43dfd8
Daniel Cazzulino <daniel@cazzulino.com>
2020-04-16 19:38:02 -0300

    Add simple utility to encode/decode a short string from an identifier
    We can use this in the future to provide a short url to uniquely identify
    users on the site (i.e. /users/1bQUI) without exposing their national
    identifiers directly.

---
+++ Encoder.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace NosAyudamos
+{
+    public class Encoder
+    {
+        [FunctionName("encode")]
+        public async Task<IActionResult> EncodeAsync([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "base62/encode")] HttpRequest req)
+        {
+            Contract.Assert(req != null);
+
+            using var reader = new StreamReader(req.Body);
+            var body = await reader.ReadToEndAsync();
+
+            if (!int.TryParse(body, out var number))
+                return new BadRequestResult();
+
+            return new OkObjectResult(Base62.Encode(number));
+        }
+
+        [FunctionName("decode")]
+        public async Task<IActionResult> DecodeAsync([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "base62/decode")] HttpRequest req)
+        {
+            Contract.Assert(req != null);
+
+            using var reader = new StreamReader(req.Body);
+            var body = await reader.ReadToEndAsync();
+
+            if (string.IsNullOrEmpty(body))
+                return new BadRequestResult();
+
+            return new OkObjectResult(Base62.Decode(body));
+        }
+    }
+}
