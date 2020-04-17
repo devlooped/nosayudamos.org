f02e32a3cc8f886e9a4d0f48b65e72939887cb29
pga <pgaliano@gmail.com>
2020-04-16 22:12:28 -0300

    Adding IBlobStorage

---
+++ Services/IBlobStorage.cs
@@ -0,0 +1,44 @@
+using System.IO;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+
+namespace NosAyudamos
+{
+    public interface IBlobStorage
+    {
+        Task UploadAsync(byte[] bytes, string containerName, string blobName);
+    }
+
+    public class BlobStorage : IBlobStorage
+    {
+        private readonly IEnviroment enviroment;
+
+        public BlobStorage(IEnviroment enviroment)
+        {
+            this.enviroment = enviroment;
+        }
+
+        public async Task UploadAsync(byte[] bytes, string containerName, string blobName)
+        {
+            var blobServiceClient = CreateBlobServiceClient();
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            if(!await containerClient.ExistsAsync())
+            {
+                containerClient = await blobServiceClient.CreateBlobContainerAsync(containerName);
+            }
+
+            var blobClient = containerClient.GetBlobClient(blobName);
+            
+            using var stream = new MemoryStream(bytes);
+            
+            await blobClient.UploadAsync(stream, true);
+        }
+
+        private BlobServiceClient CreateBlobServiceClient()
+        {
+            return new BlobServiceClient(
+                enviroment.GetVariable("StorageConnectionString"));
+        }
+    }
+}
