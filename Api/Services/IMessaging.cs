6560e0d52e3b84bc609cd7ddac56ada8fb6ddc84
Daniel Cazzulino <daniel@cazzulino.com>
2020-04-20 09:37:26 -0300

    Add required prefix whatsapp: to Twilio messaging

--- Api/Services/IMessaging.cs
+++ Api/Services/IMessaging.cs
@@ -82,8 +82,8 @@ namespace NosAyudamos
         public async Task SendTextAsync(string from, string body, string to)
         {
             var message = await MessageResource.CreateAsync(
-               from: new Twilio.Types.PhoneNumber("+" + from),
-               to: new Twilio.Types.PhoneNumber("+" + to),
+               from: new Twilio.Types.PhoneNumber("whatsapp:+" + from),
+               to: new Twilio.Types.PhoneNumber("whatsapp:+" + to),
                body: body).ConfigureAwait(false);
 
             //return message.Sid;
