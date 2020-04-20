eff71b07ee3c49276e59692fc476e73740a3caf5
Daniel Cazzulino <daniel@cazzulino.com>
2020-04-20 06:32:55 -0300

    Add function to receive MP callbacks
    Empty and just logging for now

---
+++ Api/MercadoPago.cs
@@ -0,0 +1,30 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Text.Json;
+
+namespace NosAyudamos
+{
+    class MercadoPago
+    {
+        readonly ILogger<MercadoPago> logger;
+
+        public MercadoPago(ILogger<MercadoPago> logger) => this.logger = logger;
+
+        [FunctionName("mercadopago")]
+        public async Task<IActionResult> RunAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req)
+        {
+            using var reader = new StreamReader(req.Body);
+            var body = await reader.ReadToEndAsync();
+
+            logger.LogInformation(JsonSerializer.Serialize(JsonDocument.Parse(body).RootElement, new JsonSerializerOptions { WriteIndented = true, Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping }));
+
+            return new OkObjectResult("");
+        }
+    }
+}
