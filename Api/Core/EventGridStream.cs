5277774049f65193a20b63d88c0b4235c3d7d674
Daniel Cazzulino <daniel@cazzulino.com>
2020-04-24 00:53:14 -0300

    Add eventing mechanism with abstraction on top of Merq
    The IEventStream interface makes it easy to test/mock interactions
    too so we can eventually create unit tests for flows we support,
    to implement on top of the requirement specs.

---
+++ Api/Core/EventGridStream.cs
@@ -0,0 +1,16 @@
+﻿using System.Composition;
+using Merq;
+
+namespace NosAyudamos
+{
+    [Shared]
+    class EventGridStream : EventStream
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0022:Use expression body for methods", Justification = "WIP")]
+        public override void Push<TEvent>(TEvent @event)
+        {
+            base.Push(@event);
+            // TODO: convert to EventGrid model and push it.
+        }
+    }
+}
