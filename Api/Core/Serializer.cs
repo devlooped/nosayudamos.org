98a81560e13433697e07c67b77f4144034fbc7d0
Daniel Cazzulino <daniel@cazzulino.com>
2020-04-25 04:58:12 -0300

    Add generic serializer based on Json.NET
    The built-in `System.Text.Json` does not offer the ability to deserialize using constructors, which is a showstopper since it would force us to provide parameterless constructor for deserialization and then defeating the nullability checks we could otherwise be performing.
    
    Also, it made it impossible to implement immutable entities, which are more important for the EventGrid work that's coming next.

---
+++ Api/Core/Serializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NosAyudamos.Core
+{
+    class Serializer : ISerializer
+    {
+        JsonSerializerSettings settings = new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore,
+            Formatting = Formatting.Indented,
+        };
+
+        public T Deserialize<T>(object data)
+            => JsonConvert.DeserializeObject<T>(data?.ToString() ?? throw new ArgumentNullException(nameof(data)), settings)!;
+
+        public object Serialize<T>(T value)
+            => JsonConvert.SerializeObject(value, settings);
+    }
+
+    interface ISerializer
+    {
+        object Serialize<T>(T value);
+
+        T Deserialize<T>(object data);
+    }
+}
