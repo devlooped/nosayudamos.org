cc505e18184482976ab0fa995b0a2b19aa014f6b
pga <pgaliano@gmail.com>
2020-04-27 22:55:36 -0300

    Add InteractiveAction
    InteractiveAction handles a retry pattern for actions that require user interaction and are long running

--- Api/Workflow/StartupWorkflow.cs
+++ Api/Workflow/StartupWorkflow.cs
@@ -25,6 +25,7 @@ namespace NosAyudamos
         readonly IRepositoryFactory repositoryFactory;
         readonly PersonRepository personRepository;
         readonly HttpClient httpClient;
+        readonly InteractiveAction interactiveAction;
 
         public StartupWorkflow(IEnvironment enviroment,
                             ILanguageUnderstanding languageUnderstanding,
@@ -35,96 +36,30 @@ namespace NosAyudamos
                             IRepositoryFactory repositoryFactory,
                             PersonRepository personRepository,
                             HttpClient httpClient,
+                            InteractiveAction interactiveAction,
                             ILogger<StartupWorkflow> logger) =>
-                            (this.enviroment, this.languageUnderstanding, this.messaging, this.workflowSelector, this.blobStorage, this.personRecognizer, this.repositoryFactory, this.personRepository, this.httpClient, this.logger) =
-                                (enviroment, languageUnderstanding, messaging, workflowSelector, blobStorage, personRecognizer, repositoryFactory, personRepository, httpClient, logger);
+                            (this.enviroment, this.languageUnderstanding, this.messaging, this.workflowSelector, this.blobStorage, this.personRecognizer, this.repositoryFactory, this.personRepository, this.httpClient, this.interactiveAction, this.logger) =
+                                (enviroment, languageUnderstanding, messaging, workflowSelector, blobStorage, personRecognizer, repositoryFactory, personRepository, httpClient, interactiveAction, logger);
 
-        public async Task RunAsync(MessageEvent @event)
+        public async Task RunAsync(MessageEvent @event) => await RegisterDoneeAsync((ImageMessageReceived)@event);
+
+        private async Task RegisterDoneeAsync(ImageMessageReceived @event)
         {
-            //TODO: Find person by phone number and execute person workflow
-            var message = (TextMessageReceived)@event;
+#pragma warning disable CS8634 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'class' constraint.
+            var person = await interactiveAction.ExecuteAsync(
+#pragma warning restore CS8634 // The type cannot be used as type parameter in the generic type or method. Nullability of type argument doesn't match 'class' constraint.
+                () => personRecognizer.RecognizeAsync(@event.ImageUri),
+                () => messaging.SendTextAsync(@event.To, "Dni invalido, intente de nuevo.", @event.From),
+                () => messaging.SendTextAsync(@event.To, "No pudimos procesar su dni. Nos contactaremos en breve.", @event.From));
 
-            if (Uri.TryCreate(message.Text, UriKind.Absolute, out var url))
+            if (person != null)
             {
-                await RegisterDoneeAsync(message);
-            }
-            else
-            {
-                var intents = await languageUnderstanding.GetIntentsAsync(message.Text);
+                var image = await httpClient.GetByteArrayAsync(@event.ImageUri);
 
-                if (intents.ContainsKey("help"))
-                {
-                    await messaging.SendTextAsync(
-                        message.To, "Gracias por contactarnos, envianos foto de tu DNI para registarte primero.", message.From);
-                }
-                else if (intents.ContainsKey("donate"))
-                {
-                    var workflow = workflowSelector.Value.Select(Workflow.Donor);
-                    await workflow.RunAsync(message);
-                }
-                else
-                {
-                    await messaging.SendTextAsync(
-                        message.To, "Gracias por contactarnos, querés ayudar o necesitás ayuda?", message.From);
-                }
-            }
-        }
+                await blobStorage.UploadAsync(
+                    image, enviroment.GetVariable("AttachmentsContainerName"), $"dni_{person.NationalId}.png");
 
-        private async Task RegisterDoneeAsync(TextMessageReceived message)
-        {
-            if (Uri.TryCreate(message.Text, UriKind.Absolute, out var imageUri))
-            {
-                var person = await personRecognizer.RecognizeAsync(imageUri);
-                var actionRetryRepository = repositoryFactory.Create<ActionRetryEntity>();
-
-                if (person != null)
-                {
-                    var image = await httpClient.GetByteArrayAsync(imageUri);
-
-                    await blobStorage.UploadAsync(
-                        image, enviroment.GetVariable("AttachmentsContainerName"), $"dni_{person.NationalId}.png");
-
-                    person.PhoneNumber = message.From;
-
-                    await personRepository.PutAsync(person);
-
-                    var actionRetry = await actionRetryRepository.GetAsync(message.To, Action.RecognizeId.ToString());
-
-                    if (actionRetry != null)
-                    {
-                        await actionRetryRepository.DeleteAsync(actionRetry);
-                    }
-                }
-                else
-                {
-                    var actionRetry = await actionRetryRepository.GetAsync(message.To, Action.RecognizeId.ToString());
-
-                    if (actionRetry == null)
-                    {
-                        await actionRetryRepository.PutAsync(new ActionRetryEntity(message.To, Action.RecognizeId.ToString()));
-                    }
-                    else
-                    {
-                        if (actionRetry.RetryCount < 3)
-                        {
-                            actionRetry.RetryCount += 1;
-                            await actionRetryRepository.PutAsync(actionRetry);
-                        }
-                        else
-                        {
-                            await messaging.SendTextAsync(
-                                message.To, "No pudimos procesar su dni. Nos contactaremos en breve.", message.From);
-
-                            logger.LogWarning(@"Unable to process national_id.
-Message: {@message:j}", message);
-
-                            return;
-                        }
-                    }
-
-                    await messaging.SendTextAsync(
-                        message.To, "Dni invalido, intente de nuevo.", message.From);
-                }
+                person.PhoneNumber = @event.From;
             }
         }
     }
