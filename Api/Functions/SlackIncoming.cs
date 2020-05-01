c5ed5a20c597712bf11d4c7249dee6daf54babdd
Daniel Cazzulino <daniel@cazzulino.com>
2020-05-01 05:55:18 -0300

    Add rich interaction via slack, including re-training actions

--- Api/Functions/SlackIncoming.cs
+++ Api/Functions/SlackIncoming.cs
@@ -9,6 +9,7 @@ using Newtonsoft.Json.Linq;
 using System.Net.Http;
 using Merq;
 using NosAyudamos.Events;
+using System.Globalization;
 
 namespace NosAyudamos.Functions
 {
@@ -19,15 +20,44 @@ namespace NosAyudamos.Functions
         readonly ISerializer serializer;
         readonly IEnvironment environment;
         readonly IEventStream events;
+        readonly ILanguageUnderstanding language;
         readonly HttpClient http;
         readonly ILogger<SlackIncoming> logger;
 
-        public SlackIncoming(ISerializer serializer, IEnvironment environment, IEventStream events, HttpClient http, ILogger<SlackIncoming> logger) =>
-            (this.serializer, this.environment, this.events, this.http, this.logger) = (serializer, environment, events, http, logger);
+        public SlackIncoming(ISerializer serializer, IEnvironment environment, IEventStream events, ILanguageUnderstanding language, HttpClient http, ILogger<SlackIncoming> logger) =>
+            (this.serializer, this.environment, this.events, this.language, this.http, this.logger) = (serializer, environment, events, language, http, logger);
+
+        [FunctionName("slack_interaction")]
+        public async Task<IActionResult> InteractionAsync(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "slack/interaction")] HttpRequest req)
+        {
+            using var reader = new StreamReader(req.Body);
+            var payload = await reader.ReadToEndAsync();
+            dynamic json = serializer.Deserialize<JObject>(System.Net.WebUtility.UrlDecode(payload.Substring(8)));
+
+            if (json.challenge != null)
+            {
+                return new OkObjectResult(json.challenge);
+            }
+
+            string? intent = json.actions?[0]?.value;
+            string? message = json.message?.blocks?[2]?.text?.text;
+            if (message != null && message.Trim().StartsWith("&gt;", System.StringComparison.Ordinal))
+                message = message.Trim().Substring(4);
+
+            if (message != null)
+                message = message.Trim();
+
+            // TODO: uncomment when new API is merged.
+            //await language.AddUtteranceAsync(message, intent);
+            logger.LogInformation("Training intent '{intent}' with new phrase: {phrase}", intent, message);
+
+            return new OkResult();
+        }
 
         [FunctionName("slack_incoming")]
         public async Task<IActionResult> ReceiveAsync(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "slack")] HttpRequest req)
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "slack/message")] HttpRequest req)
         {
             using var reader = new StreamReader(req.Body);
             var payload = await reader.ReadToEndAsync();
@@ -45,7 +75,9 @@ namespace NosAyudamos.Functions
             if (!string.IsNullOrEmpty(threadId))
             {
                 // Keep the original response text.
-                string text = json["event"].text;
+                string? text = json["event"]?.text;
+                if (string.IsNullOrEmpty(text))
+                    return new OkResult();
 
                 var token = environment.GetVariable("SlackToken");
                 var response = await http.GetAsync($"{ApiUrl}/conversations.replies?token={token}&channel={json["event"].channel}&ts={threadId}&pretty=1")
@@ -54,19 +86,13 @@ namespace NosAyudamos.Functions
                 var body = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
                 json = serializer.Deserialize<JObject>(body);
 
-                if (json.messages?[0]?.attachments?[0]?.fields == null)
-                    return new OkResult();
+                string? from = json.messages?[0]?.blocks?[1]?.fields?[0]?.text;
+                string? to = json.messages?[0]?.blocks?[1]?.fields?[1]?.text;
 
-                dynamic fields = json.messages[0].attachments[0].fields;
-
-                string? from = fields[0]?.value;
-                string? to = fields[1]?.value;
-
-                if (!string.IsNullOrEmpty(from) && !string.IsNullOrEmpty(to))
-                    events.Push(new MessageSent(to, from, text));
+                if (!string.IsNullOrEmpty(from) && !string.IsNullOrEmpty(to) && !string.IsNullOrEmpty(text))
+                    events.Push(new MessageSent(to.Substring(to.LastIndexOf(':') + 1).Trim(), from.Substring(from.LastIndexOf(':') + 1).Trim(), text));
             }
 
-            System.Console.WriteLine();
             return new OkResult();
         }
     }
