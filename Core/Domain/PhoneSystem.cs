d2d0683f08d313f180f259db8e32c35c1c0aa344
Daniel Cazzulino <daniel@cazzulino.com>
2020-05-19 04:22:50 -0300

    Persist current system phone number in use by user
    This will allow us to remove the From altogether, since the messaging
    systems will just reply from the same channel/number used by the
    user in their initial contact.

---
+++ Core/Domain/PhoneSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NosAyudamos
+{
+    /// <summary>
+    /// Maps the phone system used by a user, by phone number
+    /// </summary>
+    public class PhoneSystem
+    {
+        public PhoneSystem(string userNumber, string systemNumber)
+        {
+            UserNumber = userNumber;
+            SystemNumber = systemNumber;
+        }
+
+        [Key]
+        public string UserNumber { get; set; }
+        public string SystemNumber { get; set; }
+    }
+}
