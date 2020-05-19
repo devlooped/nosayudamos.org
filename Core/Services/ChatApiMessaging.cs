1a6720c084cfb84b352d7a8ff2068069ae024f8b
Daniel Cazzulino <daniel@cazzulino.com>
2020-05-19 04:25:47 -0300

    Allow sending images with subtext
    We borrow markdown syntax for images: `![subtext for image](imageUrl)`.
    
    Currently scoped to a hardcoded gif we used for sending captchas. Need to generalize it.

--- Core/Services/ChatApiMessaging.cs
+++ Core/Services/ChatApiMessaging.cs
@@ -10,19 +10,50 @@ namespace NosAyudamos
     {
         readonly HttpClient httpClient;
         readonly ISerializer serializer;
-        readonly Lazy<string> apiUrl;
+        readonly IEnvironment environment;
 
-        public ChatApiMessaging(IEnvironment enviroment, HttpClient httpClient, ISerializer serializer)
+        public ChatApiMessaging(IEnvironment environment, HttpClient httpClient, ISerializer serializer)
         {
             this.httpClient = httpClient;
             this.serializer = serializer;
-            apiUrl = new Lazy<string>(() => enviroment.GetVariable("ChatApiUrl"));
+            this.environment = environment;
         }
 
         public async Task SendTextAsync(string from, string body, string to)
         {
-            using var content = new StringContent(serializer.Serialize(new { phone = to.TrimStart('+'), body }), Encoding.UTF8, "application/json");
-            await httpClient.PostAsync(apiUrl.Value, content).ConfigureAwait(false);
+            var baseUrl = environment.GetVariable("ChatApiBaseUrl").TrimEnd('/');
+            var token = environment.GetVariable("ChatApiToken");
+
+            // Markdown-like image format to allow for text + url
+            if (body.StartsWith("![", StringComparison.Ordinal))
+            {
+                var text = body.Substring(2, body.LastIndexOf(']') - 2);
+                var url = body.Substring(body.LastIndexOf('(')).TrimStart('(').TrimEnd(')');
+
+                // Post as file.
+                var bytes = await httpClient.GetByteArrayAsync(url);
+                var msg = new
+                {
+                    phone = to.TrimStart('+'),
+                    body = "data:image/gif;base64," + Convert.ToBase64String(bytes),
+                    filename = "captcha.gif",
+                    caption = text,
+                };
+
+                using var content = new StringContent(serializer.Serialize(msg), Encoding.UTF8, "application/json");
+                await httpClient.PostAsync($"{baseUrl}/sendFile?token={token}", content).ConfigureAwait(false);
+            }
+            else
+            {
+                var msg = new 
+                { 
+                    phone = to.TrimStart('+'), 
+                    body 
+                };
+
+                using var content = new StringContent(serializer.Serialize(msg), Encoding.UTF8, "application/json");
+                await httpClient.PostAsync($"{baseUrl}/sendMessage?token={token}", content).ConfigureAwait(false);
+            }
         }
     }
 }
