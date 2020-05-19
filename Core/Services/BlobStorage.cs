c804405b2c7fb5a2f8d643c10cb952b2e519ca51
Daniel Cazzulino <daniel@cazzulino.com>
2020-05-19 04:28:40 -0300

    Add blob metadata saving and retrieval
    This can be used to annotate the blobs with metadata used upon retrieval to customize its processing.

--- Core/Services/BlobStorage.cs
+++ Core/Services/BlobStorage.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Azure.Storage.Blobs;
@@ -6,11 +8,47 @@ namespace NosAyudamos
 {
     class BlobStorage : IBlobStorage
     {
+        static readonly Dictionary<string, string> EmptyMetadata = new Dictionary<string, string>();
         readonly IEnvironment enviroment;
 
         public BlobStorage(IEnvironment enviroment) => this.enviroment = enviroment;
 
-        public async Task UploadAsync(byte[] bytes, string containerName, string blobName)
+        public async Task<IDictionary<string, string>> GetMetadataAsync(string containerName, string blobName)
+        {
+            var blobServiceClient = CreateBlobServiceClient();
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            if (!await containerClient.ExistsAsync())
+            {
+                return EmptyMetadata;
+            }
+
+            var blobClient = containerClient.GetBlobClient(blobName);
+
+            if (!await blobClient.ExistsAsync())
+                return EmptyMetadata;
+
+            return new Dictionary<string, string>(
+                (await blobClient.GetPropertiesAsync()).Value.Metadata,
+                StringComparer.OrdinalIgnoreCase);
+        }
+
+        public async Task SetMetadataAsync(string containerName, string blobName, IDictionary<string, string> blobMetadata)
+        {
+            var blobServiceClient = CreateBlobServiceClient();
+            var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+            if (!await containerClient.ExistsAsync())
+                return;
+
+            var blobClient = containerClient.GetBlobClient(blobName);
+            if (!await blobClient.ExistsAsync())
+                return;
+
+            await blobClient.SetMetadataAsync(blobMetadata);
+        }
+
+        public async Task<Uri> UploadAsync(byte[] bytes, string containerName, string blobName, IDictionary<string, string>? blobMetadata = null)
         {
             var blobServiceClient = CreateBlobServiceClient();
             var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
@@ -23,8 +61,12 @@ namespace NosAyudamos
             var blobClient = containerClient.GetBlobClient(blobName);
 
             using var stream = new MemoryStream(bytes);
-
             await blobClient.UploadAsync(stream, true).ConfigureAwait(false);
+
+            if (blobMetadata != null)
+                await blobClient.SetMetadataAsync(blobMetadata);
+
+            return blobClient.Uri;
         }
 
         BlobServiceClient CreateBlobServiceClient() => new BlobServiceClient(enviroment.GetVariable("AzureWebJobsStorage"));
@@ -32,6 +74,8 @@ namespace NosAyudamos
 
     interface IBlobStorage
     {
-        Task UploadAsync(byte[] bytes, string containerName, string blobName);
+        Task<IDictionary<string, string>> GetMetadataAsync(string containerName, string blobName);
+        Task SetMetadataAsync(string containerName, string blobName, IDictionary<string, string> blobMetadata);
+        Task<Uri> UploadAsync(byte[] bytes, string containerName, string blobName, IDictionary<string, string>? blobMetadata = null);
     }
 }
