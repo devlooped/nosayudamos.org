0adedade643fd67ba0982f526401c84b66cd77d5
Daniel Cazzulino <daniel@cazzulino.com>
2020-05-24 19:20:54 -0300

    Make person's date of birth an actual date
    Also add Age so we can easily render that in messages/logs.

--- Features/Constants.cs
+++ Features/Constants.cs
@@ -1,4 +1,6 @@
-﻿namespace NosAyudamos
+﻿using System;
+
+namespace NosAyudamos
 {
     static class Constants
     {
@@ -8,9 +10,10 @@
             public const string FirstName = "Fernand@";
             public const string LastName = nameof(Donor);
             public const string PhoneNumber = "111";
+            public static DateTime DateOfBirth { get; } = new DateTime(2000, 3, 15);
 
             public static Person Create() =>
-                new Person(Id, FirstName, LastName, PhoneNumber);
+                new Person(Id, FirstName, LastName, PhoneNumber, Role.Donor, DateOfBirth);
         }
 
         public static class Donee
@@ -19,9 +22,10 @@
             public const string FirstName = "Marcel@";
             public const string LastName = nameof(Donee);
             public const string PhoneNumber = "222";
+            public static DateTime DateOfBirth { get; } = new DateTime(2000, 3, 15);
 
             public static Person Create() =>
-                new Person(Id, FirstName, LastName, PhoneNumber);
+                new Person(Id, FirstName, LastName, PhoneNumber, Role.Donee, DateOfBirth);
         }
 
         public static class System
