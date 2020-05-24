93674beced4b4ef3fa6988d050aac823fa227328
Daniel Cazzulino <daniel@cazzulino.com>
2020-05-24 19:09:04 -0300

    Simplify and unify the event grid handlers
    By generalizing the invocation of the IEventHandler<T> for
    various message types, we make the EventGrid "facade"
    much simpler and easier to maintain (and reason about).
    
    It's now clearer than ever that we're just using the EventGrid
    as a persistent/dispatcher/retry mechanism, while all the
    logic lives in Core.

---
+++ Api/EventGrid/EventHandlers.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventGrid.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.EventGrid;
+
+namespace NosAyudamos.EventGrid
+{
+    class EventHandlers
+    {
+        readonly ISerializer serializer;
+        readonly IServiceProvider services;
+
+        public EventHandlers(ISerializer serializer, IServiceProvider services)
+            => (this.serializer, this.services)
+            = (serializer, services);
+
+
+        /// <summary>
+        /// Initial handler of uncategorized incoming messages from event grid 
+        /// callbacks into our azure function.
+        /// <see cref="IEventHandler{TEvent}"/>.
+        /// </summary>
+        [FunctionName("message-received")]
+        public Task MessageReceivedAsync([EventGridTrigger] EventGridEvent e) => HandleAsync(e.GetData<MessageReceived>(serializer));
+
+        [FunctionName("unknown-message")]
+        public Task UnknownMessageAsync([EventGridTrigger] EventGridEvent e) => HandleAsync(e.GetData<UnknownMessageReceived>(serializer));
+
+        [FunctionName("message-sent")]
+        public Task MessageSentAsync([EventGridTrigger] EventGridEvent e) => HandleAsync(e.GetData<MessageSent>(serializer));
+
+        [FunctionName("person-registered")]
+        public Task PersonRegisteredAsync([EventGridTrigger] EventGridEvent e) => HandleAsync(e.GetData<PersonRegistered>(serializer));
+
+        [FunctionName("slack-message-sent")]
+        public Task SlackMessageSentAsync([EventGridTrigger] EventGridEvent e) => HandleAsync(e.GetData<SlackMessageSent>(serializer));
+
+        async Task HandleAsync<TEvent>(TEvent e)
+        {
+            var handlers = (IEnumerable<IEventHandler<TEvent>>)services.GetService(typeof(IEnumerable<IEventHandler<TEvent>>));
+            foreach (var handler in handlers)
+            {
+                await handler.HandleAsync(e);
+            }
+        }
+    }
+}
