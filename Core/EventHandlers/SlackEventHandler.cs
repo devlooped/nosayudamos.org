2932460a958c147fd7ba8c2530c4d6dfddb0a5ea
Daniel Cazzulino <daniel@cazzulino.com>
2020-05-24 19:44:02 -0300

    Centralize, unify and simplify slack messaging usage
    Rather than having various event handlers decide how to
    send messages to slack, we make the same design choice
    as with messaging: we create a `SlackMessageSent`  and
    have it sent via the EventGrid, where a single handler deals
    with it and how it's sent, in a uniform way (i.e. detemrining
    channel and thread id to use).

---
+++ Core/EventHandlers/SlackEventHandler.cs
@@ -0,0 +1,230 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Humanizer;
+using Slack.Webhooks;
+using Slack.Webhooks.Blocks;
+using Slack.Webhooks.Elements;
+using Slack.Webhooks.Interfaces;
+
+namespace NosAyudamos
+{
+    class SlackEventHandler : 
+        IEventHandler<TaxStatusRejected>, 
+        IEventHandler<TaxStatusAccepted>, 
+        IEventHandler<UnknownMessageReceived>
+    {
+        readonly IEnvironment environment;
+        readonly IPersonRepository repository;
+        readonly IEntityRepository<PhoneSystem> phoneRepo;
+        readonly IEventStreamAsync events;
+        readonly ILanguageUnderstanding language;
+
+        public SlackEventHandler(
+            IEnvironment environment, IPersonRepository repository,
+            IEntityRepository<PhoneSystem> phoneRepo, IEventStreamAsync events, 
+            ILanguageUnderstanding language)
+            => (this.environment, this.repository, this.phoneRepo, this.events, this.language)
+            = (environment, repository, phoneRepo, events, language);
+
+        public async Task HandleAsync(UnknownMessageReceived e)
+        {
+            if (environment.IsDevelopment() && !environment.GetVariable("SendToSlackInDevelopment", false))
+                return;
+
+            var from = "Unknown";
+            if (e.PersonId != null &&
+                await repository.GetAsync(e.PersonId) is Person person &&
+                person != null)
+            {
+                from = person.FirstName + " " + person.LastName;
+            }
+
+            var intents = await language.GetIntentsAsync(e.Body);
+            var context = new StringBuilder();
+            if (intents.TryGetValue("Help", out var help))
+                context = context.Append(":pray: ").Append(help.Score?.ToString("0.##", CultureInfo.CurrentCulture));
+
+            if (intents.TryGetValue("Donate", out var donate))
+                context = context.Append(":money_with_wings: ").Append(donate.Score?.ToString("0.##", CultureInfo.CurrentCulture));
+
+            context = context.Append(" by ").Append(from).Append(", ").Append(e.When.Humanize());
+            var toEmoji = e.PhoneNumber == environment.GetVariable("ChatApiNumber").TrimStart('+') ? ":whatsapp:" : ":twilio:";
+            var map = await phoneRepo.GetAsync(e.PhoneNumber);
+
+            var message = new SlackMessage
+            {
+                Username = "nosayudamos",
+                Blocks = new List<Block>
+                {
+                    new Divider(),
+                    new Section
+                    {
+                        BlockId = "sender",
+                        Fields = new List<TextObject>
+                        {
+                            new TextObject($":unknown: {e.PhoneNumber}") { Emoji = true },
+                            new TextObject($"{toEmoji} {map?.SystemNumber}") { Emoji = true },
+                        }
+                    },
+                    new Section
+                    {
+                        BlockId = "body",
+                        Text = new TextObject($"> {e.Body}") { Type = TextObject.TextType.Markdown, Emoji = true },
+                    },
+                    new Context
+                    {
+                        Elements = new List<IContextElement>
+                        {
+                            new TextObject(context.ToString().Trim()) { Emoji = true },
+                        }
+                    },
+                    new Actions
+                    {
+                        Elements = new List<IActionElement>
+                        {
+                            new Button
+                            {
+                                Text = new TextObject("Train as :pray:") { Emoji = true },
+                                Style = "primary",
+                                Value = "help"
+                            },
+                            new Button
+                            {
+                                Text = new TextObject("Train as :money_with_wings:") { Emoji = true },
+                                Value = "donate"
+                            },
+                            new Button
+                            {
+                                Text = new TextObject("Retry :repeat:") { Emoji = true },
+                                Value = "retry"
+                            },
+                            new Button
+                            {
+                                Text = new TextObject("Pause :automation_pause:") { Emoji = true },
+                                Value = "pause"
+                            },
+                            new Button
+                            {
+                                Text = new TextObject("Resume :automation_resume:") { Emoji = true },
+                                Value = "resume"
+                            },
+                        }
+                    }
+                },
+            };
+
+            await events.PushAsync(new SlackMessageSent(e.PhoneNumber, message.AsJson())).ConfigureAwait(false);
+        }
+
+        public async Task HandleAsync(TaxStatusAccepted e)
+        {
+            var person = await repository.GetAsync(e.SourceId!);
+            if (person == null)
+                return;
+
+            await events.PushAsync(new MessageSent(
+                person.PhoneNumber, 
+                Strings.UI.Donee.Welcome(person.FirstName.Split(' ').First(), person.Sex == Sex.Male ? "o" : "a")))
+                .ConfigureAwait(false);
+
+            var message = new SlackMessage
+            {
+                Username = "nosayudamos",
+                Blocks = new List<Block>
+                {
+                    new Divider(),
+                    new Section
+                    {
+                        BlockId = "sender",
+                        Fields = new List<TextObject>
+                        {
+                            new TextObject($":approved: 5491159278282") { Emoji = true },
+                            new TextObject($"{person.FirstName} {person.LastName} ({person.Age}), <https://www.cuitonline.com/search.php?q={e.SourceId}|{e.TaxId}>")
+                            {
+                                Type = TextObject.TextType.Markdown
+                            },
+                        }
+                    },
+                    new Actions
+                    {
+                        Elements = new List<IActionElement>
+                        {
+                            new Button
+                            {
+                                Text = new TextObject("Pause :automation_pause:") { Emoji = true },
+                                Value = "pause"
+                            },
+                            new Button
+                            {
+                                Text = new TextObject("Resume :automation_resume:") { Emoji = true },
+                                Value = "resume"
+                            },
+                        }
+                    }
+                },
+            };
+
+            await events.PushAsync(new SlackMessageSent(person.PhoneNumber, message.AsJson()));
+        }
+
+        public async Task HandleAsync(TaxStatusRejected e)
+        {
+            var person = await repository.GetAsync(e.SourceId!);
+            if (person == null)
+                return;
+
+            var name = person.FirstName.Split(' ').First();
+            var body = e.Reason switch
+            {
+                TaxStatusRejectedReason.NotApplicable => Strings.UI.Donee.NotApplicable(name),
+                TaxStatusRejectedReason.HasIncomeTax => Strings.UI.Donee.HasIncomeTax(name),
+                TaxStatusRejectedReason.HighCategory => Strings.UI.Donee.HighCategory(name),
+                _ => Strings.UI.Donee.Rejected,
+            };
+
+            await events.PushAsync(new MessageSent(person.PhoneNumber, body));
+            
+            var message = new SlackMessage
+            {
+                Username = "nosayudamos",
+                Blocks = new List<Block>
+                {
+                    new Divider(),
+                    new Section
+                    {
+                        BlockId = "sender",
+                        Fields = new List<TextObject>
+                        {
+                            new TextObject($":rejected: 5491159278282") { Emoji = true },
+                            new TextObject($"{person.FirstName} {person.LastName} ({person.Age}), <https://www.cuitonline.com/search.php?q={e.SourceId}|{e.TaxId}>")
+                            {
+                                Type = TextObject.TextType.Markdown
+                            },
+                        }
+                    },
+                    new Actions
+                    {
+                        Elements = new List<IActionElement>
+                        {
+                            new Button
+                            {
+                                Text = new TextObject("Pause :automation_pause:") { Emoji = true },
+                                Value = "pause"
+                            },
+                            new Button
+                            {
+                                Text = new TextObject("Resume :automation_resume:") { Emoji = true },
+                                Value = "resume"
+                            },
+                        }
+                    }
+                },
+            };
+
+            await events.PushAsync(new SlackMessageSent(person.PhoneNumber, message.AsJson()));
+        }
+    }
+}
---
+++ UnitTests/SlackTests.cs
@@ -0,0 +1,178 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.Cosmos.Table;
+using Slack.Webhooks;
+using Slack.Webhooks.Blocks;
+using Slack.Webhooks.Elements;
+using Slack.Webhooks.Interfaces;
+using Xunit;
+
+namespace NosAyudamos
+{
+    public class SlackTests
+    {
+        IEnvironment environment;
+
+        public SlackTests() => environment = new Environment();
+
+        [Fact]
+        public async Task SendRejected()
+        {
+            var person = Constants.Donee.Create();
+            await SendMessageAsync(new SlackMessage
+            {
+                Username = "nosayudamos",
+                Blocks = new List<Block>
+                {
+                    new Divider(),
+                    new Section
+                    {
+                        BlockId = "sender",
+                        Fields = new List<TextObject>
+                        {
+                            new TextObject($":rejected: 5491159278282") { Emoji = true },
+                            new TextObject($"{person.FirstName} {person.LastName} ({person.Age}), <https://www.cuitonline.com/constancia/inscripcion/20251885398|CUIT sin Monotributo>") 
+                            { 
+                                Type = TextObject.TextType.Markdown 
+                            },
+                        }
+                    },
+                },
+            });
+
+            await SendMessageAsync(new SlackMessage
+            {
+                Username = "nosayudamos",
+                Blocks = new List<Block>
+                {
+                    new Divider(),
+                    new Section
+                    {
+                        BlockId = "sender",
+                        Fields = new List<TextObject>
+                        {
+                            new TextObject($":rejected: 5491159278282") { Emoji = true },
+                            new TextObject($"{person.FirstName} {person.LastName} ({person.Age}), <https://www.cuitonline.com/constancia/inscripcion/20251885398|CUIT paga ganancias>")
+                            {
+                                Type = TextObject.TextType.Markdown
+                            },
+                        }
+                    },
+                },
+            });
+
+            await SendMessageAsync(new SlackMessage
+            {
+                Username = "nosayudamos",
+                Blocks = new List<Block>
+                {
+                    new Divider(),
+                    new Section
+                    {
+                        BlockId = "sender",
+                        Fields = new List<TextObject>
+                        {
+                            new TextObject($":rejected: 5491159278282") { Emoji = true },
+                            new TextObject($"{person.FirstName} {person.LastName} ({person.Age}), <https://www.cuitonline.com/constancia/inscripcion/20251885398|Monotributo categoría D>")
+                            {
+                                Type = TextObject.TextType.Markdown
+                            },
+                        }
+                    },
+                },
+            });
+        }
+
+        [Fact]
+        public async Task SendApproved()
+        {
+            var person = Constants.Donee.Create();
+            await SendMessageAsync(new SlackMessage
+            {
+                Username = "nosayudamos",
+                Blocks = new List<Block>
+                {
+                    new Divider(),
+                    new Section
+                    {
+                        BlockId = "sender",
+                        Fields = new List<TextObject>
+                        {
+                            new TextObject($":approved: 5491159278282") { Emoji = true },
+                            new TextObject($"{person.FirstName} {person.LastName} ({person.Age})"),
+                        }
+                    },
+                },
+            });
+        }
+
+        [Fact]
+        public async Task SendUnknown()
+        {
+            await SendMessageAsync(new SlackMessage
+            {
+                Username = "nosayudamos",
+                Blocks = new List<Block>
+                {
+                    new Divider(),
+                    new Section
+                    {
+                        BlockId = "sender",
+                        Fields = new List<TextObject>
+                        {
+                            new TextObject($":unknown: {Constants.Donee.PhoneNumber}") { Emoji = true },
+                            new TextObject($":whatsapp: {Constants.System.PhoneNumber}") { Emoji = true },
+                        }
+                    },
+                    new Section
+                    {
+                        BlockId = "body",
+                        Text = new TextObject($"> Gracias vieja!") { Type = TextObject.TextType.Markdown },
+                    },
+                    new Context
+                    {
+                        Elements = new List<IContextElement>
+                        {
+                            new TextObject($":help: 0.54 :donate: 0.23 by {Constants.Donee.FirstName} {Constants.Donee.LastName} 5 minutes ago.") { Emoji = true },
+                        }
+                    },
+                    new Actions
+                    {
+                        Elements = new List<IActionElement>
+                        {
+                            new Button
+                            {
+                                Text = new TextObject("Train as :help:") { Emoji = true },
+                                Style = "primary",
+                                Value = "help"
+                            },
+                            new Button
+                            {
+                                Text = new TextObject("Train as :donate:") { Emoji = true },
+                                Value = "donate"
+                            },
+                            new Button
+                            {
+                                Text = new TextObject("Retry :retry:") { Emoji = true },
+                                Value = "retry"
+                            },
+                        }
+                    }
+                },
+            });
+        }
+
+        async Task SendMessageAsync(SlackMessage message)
+        {
+            using var http = new HttpClient();
+            var handler = new SlackMessageSentHandler(environment,
+                new EntityRepository<PhoneThread>(CloudStorageAccount.DevelopmentStorageAccount, new Serializer()),
+                http);
+
+            await handler.HandleAsync(new SlackMessageSent(Constants.Donee.PhoneNumber, message.AsJson()));
+            // Subsequent one replies to the prior one.
+            await handler.HandleAsync(new SlackMessageSent(Constants.Donee.PhoneNumber, "{ \"text\": \"Got it!\" }"));
+        }
+    }
+}
