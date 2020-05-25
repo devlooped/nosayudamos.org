76120ab239a4f93d173b4fc8b3156c28ef0c09d6
Daniel Cazzulino <daniel@cazzulino.com>
2020-05-25 15:32:50 -0300

    Add automation pause/resume behavior
    Whenever a user starts interacting with support via slack,
    the automated processing via the workflows/pipeline is
    wasteful and could even result in unexpected replies from
    the system when the user is expecting a response from (or
    responding to) a human. So we add a unified mechanism
    to all messages sent to slack to offer the Pause/Resume
    actions, which quickly set the given phone number as
    non-automated, meaning all messages incoming for it are
    forwarded as-is to Slack (to the given ongoing thread) and
    no further processing is performed.
    
    Note that this is decoupled from a registered Person since
    we might want to switch to this mode when helping a new
    user get registered in the first place.

--- Core/Domain/PhoneSystem.cs
+++ Core/Domain/PhoneSystem.cs
@@ -16,5 +16,13 @@ namespace NosAyudamos
         [Key]
         public string UserNumber { get; set; }
         public string SystemNumber { get; set; }
+
+        /// <summary>
+        /// Allows pausing the regular message processing for interactions 
+        /// with this phone number. This allows rich follow-up via Slack 
+        /// without interferring with the normal processing of incoming 
+        /// messages.
+        /// </summary>
+        public bool? AutomationPaused { get; set; }
     }
 }
