3b991a683da1477e27a473c7b01f9fd1ad78fad6
Daniel Cazzulino <daniel@cazzulino.com>
2020-05-29 18:45:01 -0300

    Add explicit tax approval behavior
    This allows a pending/rejected status to be explicitly overriden
    when manual validation was performed.

--- Core/Domain/TaxStatus.cs
+++ Core/Domain/TaxStatus.cs
@@ -2,8 +2,25 @@
 {
     public enum TaxStatus
     {
+        /// <summary>
+        /// Tax status is not yet known.
+        /// </summary>
         Unknown,
+        /// <summary>
+        /// Tax status was explicitly approved by an approver.
+        /// </summary>
+        Approved,
+        /// <summary>
+        /// Tax status was validated automatically based on 
+        /// rules applied to the <see cref="TaxIdKind"/> and 
+        /// <see cref="TaxCategory"/> of the user.
+        /// </summary>
         Validated,
+        /// <summary>
+        /// Tax status was rejected automatically based on 
+        /// rules applied to the <see cref="TaxIdKind"/> and 
+        /// <see cref="TaxCategory"/> of the user.
+        /// </summary>
         Rejected,
     }
 }
---
+++ Core/Slack/ApproveDoneeProcessor.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Slack.Webhooks;
+
+namespace NosAyudamos.Slack
+{
+    class ApproveDoneeProcessor : ISlackPayloadProcessor
+    {
+        readonly IEnvironment env;
+        readonly IPersonRepository peopleRepo;
+        readonly IEventStreamAsync events;
+        readonly HttpClient http;
+
+        public ApproveDoneeProcessor(IEnvironment env, 
+            IPersonRepository peopleRepo,
+            IEventStreamAsync events, HttpClient http)
+            => (this.env, this.peopleRepo, this.events, this.http)
+            = (env, peopleRepo, events, http);
+
+        public bool AppliesTo(JObject payload) =>
+            (string?)payload["type"] == "block_actions" &&
+            ((string?)payload.SelectToken("$.actions[0].value") == "approve");
+
+        public async Task ProcessAsync(JObject payload)
+        {
+            var userId = (string?)payload["user"]?["id"];
+            var sender = payload.GetSender();
+
+            if (userId == null || sender == null)
+                return;
+
+            var userName = await http.ResolveUserAsync(env, userId);
+            var person = await peopleRepo.FindAsync(sender, readOnly: false);
+
+            if (person == null)
+                return;
+
+            person.ApproveTaxStatus(userName);
+            await peopleRepo.PutAsync(person);
+
+            await events.PushAsync(new SlackMessageSent(sender, new SlackMessage
+            {
+                Text = $"Ok <@{userId}>, user has been approved",
+            }.AsJson()));
+        }
+    }
+}
