1b337aea1c6c5cd4687f034c6d867219be6c3a09
Daniel Cazzulino <daniel@cazzulino.com>
2020-06-07 17:18:14 -0300

    Add QRCode tests

---
+++ UnitTests/QRCoreTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Http;
+using Xunit;
+using ZXing;
+
+namespace NosAyudamos
+{
+    public class QRCoreTests
+    {
+        [Fact]
+        public void CanReadImageWithQR()
+        {
+            var writer = new BarcodeWriterGeneric
+            {
+                Format = BarcodeFormat.QR_CODE,
+            };
+
+            var data = Guid.NewGuid().ToString();
+
+            using var bitmap = writer.WriteAsBitmap(data);
+            using var mem = new MemoryStream();
+            bitmap.Save(mem, ImageFormat.Png);
+
+            using var http = new HttpClient();
+            var qr = new QRCode(http);
+
+            var result = qr.Decode(mem.ToArray());
+
+            Assert.NotNull(result);
+            Assert.Equal(data, result);
+        }
+
+        [Fact]
+        public void NonQRImageReturnsNullString()
+        {
+            using var http = new HttpClient();
+            var qr = new QRCode(http);
+
+            var result = qr.Decode(File.ReadAllBytes("Content\\logo.png"));
+
+            Assert.Null(result);
+        }
+    }
+}
