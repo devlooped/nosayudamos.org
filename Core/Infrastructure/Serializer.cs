ee69e2542f898ee9f51dec10d42a836a0d02d66f
Daniel Cazzulino <daniel@cazzulino.com>
2020-07-15 12:13:39 -0300

    Add flat dictionary payload serialization
    This allows us to post telemetry properties for
    rich event payloads.

--- Core/Infrastructure/Serializer.cs
+++ Core/Infrastructure/Serializer.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Composition;
 using System.Globalization;
+using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 
 namespace NosAyudamos
 {
@@ -25,6 +28,21 @@ namespace NosAyudamos
             NullValueHandling = NullValueHandling.Ignore,
         };
 
+        public IDictionary<string, string> Deserialize(string data)
+        {
+            var json = (JObject)JsonConvert.DeserializeObject(data, settings)!;
+            var results = json
+                .Descendants()
+                .Where(token => !token.Children().Any())
+                .Aggregate(new Dictionary<string, string>(), (props, token) =>
+                {
+                    props.Add(token.Path, token.ToString());
+                    return props;
+                });
+
+            return results;
+        }
+
         public object Deserialize(string data, Type type)
             => JsonConvert.DeserializeObject(data ?? throw new ArgumentNullException(nameof(data)), type, settings)!;
 
@@ -40,6 +58,8 @@ namespace NosAyudamos
         string Serialize(object value);
 
         T Deserialize<T>(string data);
+
+        IDictionary<string, string> Deserialize(string data);
 
         object Deserialize(string data, Type type);
     }
---
+++ UnitTests/SerializerTests.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace NosAyudamos
+{
+    public class SerializerTests
+    {
+        [Fact]
+        public void CanSerializeToFlatDictionary()
+        {
+            var json = JObject.Parse(File.ReadAllText("SerializerTests.json"));
+
+            var data = new Serializer().Deserialize(File.ReadAllText("SerializerTests.json"));
+
+            Assert.Equal("foo@bar.com", data["collector.email"].ToString());
+            Assert.Equal("22222222", data["collector.identification.number"].ToString());
+            Assert.Equal("not_specified", data["payer.shipping_modes[1]"].ToString());
+        }
+    }
+}
